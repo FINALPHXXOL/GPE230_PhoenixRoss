e74b38de4e8e41686da215a2c343934712181575
Phxxol <118862390+Phxxol@users.noreply.github.com>
2023-05-11 15:33:27 -0700

    Project Milestone 0
    Has a Main level and a character avatar.

---
+++ Source/GPE230_PhoenixRoss/GPE230_PhoenixRossGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GPE230_PhoenixRossGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPE230_PHOENIXROSS_API AGPE230_PhoenixRossGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
